2261fd2f06cd4a5ea63fd896549c3fbfde3df384
Jeo <jeo961@gmail.com>
2021-04-21 15:10:10 -0400

    abstract and interface

---
+++ April bootcamp Repo/02-OOP/Interfaces/Interfaces/IAnimal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    interface IAnimal
+    {
+        void AnimalSound();
+        void Eat();
+        void Move();
+        void Sleep();
+    }
+}
