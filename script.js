020e1d786f50bffca90693aba5975be7457d2ebd
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-05-20 06:34:00 -0400

    Lab completed - moving on to ext chal

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 
 let itemsAdded = [];
 
-function AddJellybeans(){
-   let jellyName = "Jellybeans";
-   let jellyPrice = 5.00;
-   let newItem = {
-       name: jellyName,
-       price: jellyPrice
-   }
-   itemsAdded.push(newItem);
+function AddJellybeans() {
+    let jellyName = "Jellybeans";
+    let jellyPrice = 5.00;
+    let newItem = {
+        name: jellyName,
+        price: jellyPrice
+    }
+    itemsAdded.push(newItem);
 }
-function AddSneakers(){
+    //alert(itemsAdded[0].name);
+
+function AddSneakers() {
     let sneakerName = "Sneakers";
     let sneakerPrice = 3.00;
     let newItem = {
@@ -18,24 +20,50 @@ function AddSneakers(){
         price: sneakerPrice
     }
     itemsAdded.push(newItem);
- }
-
- function AddMilkeyWay(){
-     let milkeywayName = "MilkeyWay";
-     let milkeywayPrice = 2.00;
-     let newItem = {
-         name: milkeywayName,
-         price: milkeywayPrice
-     }
-     itemsAdded.push(newItem);
-  }
-  let sum = 0;
-function DisplayMyItems(){
-    for(let i=0; i<itemsAdded.length; i++) {
-        console.log(itemsAdded[i].name);
-        sum =+ itemsAdded[i].price
-        console.log(sum);
-    }
 }
 
-console.log(sum);
+function AddMilkeyWay() {
+    let milkeywayName = "Milkey Way";
+    let milkeywayPrice = 2.00;
+    let newItem = {
+        name: milkeywayName,
+        price: milkeywayPrice
+    }
+    itemsAdded.push(newItem);
+}
+
+function HersheyBar() {
+    let itemName = "Hershey Bar";
+    let itemPrice = 2.00;
+    let newItem = {
+        name: itemName,
+        price: itemPrice
+    }
+    itemsAdded.push(newItem);
+}
+
+function AddHotTamales() {
+    let itemName = "Hot Tamales";
+    let itemPrice = 2.00;
+    let newItem = {
+        name: itemName,
+        price: itemPrice
+    }
+    itemsAdded.push(newItem);
+}
+
+
+let grandTotal = 0;
+function DisplayMyItems() {
+    for (let i = 0; i < itemsAdded.length; i++) {
+        let nameDisp = itemsAdded[i].name
+        let priceDisp = itemsAdded[i].price
+
+        grandTotal += priceDisp;
+        console.log(`${nameDisp}: $${priceDisp}`);
+
+
+        //alert(`Grand Total: $${grandTotal}`);
+        console.log(`Grand Total: $${grandTotal}`);
+    }
+}
